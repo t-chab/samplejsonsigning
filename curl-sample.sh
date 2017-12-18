c59cbeb8e08be35fcd92b38580cc3883b5a5ac6a
Thomas Chabaud <tchabaud@users.noreply.github.com>
2017-12-18 09:55:28 +0100

    Delete curl-sample.sh

--- curl-sample.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-curl -vvv --data "{ \"header\": { \"alg\": \"HS256\" }, \
-    \"signature\": \"eyAKICAgICAgICAiYWxnIjogIlJTMjU2IgogICAgfQ==\" }, \
-    \"protected\": \"eyAKICAgICAgICAiYWxnIjogIlJTMjU2IgogICAgfQ==\", \
-    \"payload\": \"eyAidmFsMSI6ICJ1biIsICJ2YWwyIjogIjIiIH0=\" }" \
-    -H "Content-Type: application/json" \
-    "http://localhost:13000/jaxrs-service/hello/jsonBean"
